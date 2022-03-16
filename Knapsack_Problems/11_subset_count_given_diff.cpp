19b048b0096f636acb172cfe0c0786775bbfb969
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-03-16 07:46:58 +0530

    D{

--- Knapsack_Problems/11_subset_count_given_diff.cpp
+++ Knapsack_Problems/11_subset_count_given_diff.cpp
@@ -4,39 +4,29 @@ using namespace std;
 #define pii vector<pair<int,int>>
 int t[1001][1001];
 
-int subsetcount(int arr[],int n,int sum1){
+int subsetcount(int arr[],int n,int sum){
     for(int i=0; i<=n; i++){
-        for(int j=0; j<=sum1; j++){
+        for(int j=0; j<=sum; j++){
             if(i==0){
                 t[0][j]=0;
             }
             if(j==0){
-                t[i][j]=1;
+                t[i][0]=1;
             }
         }
     }
-    for(int i=0; i<=n; i++){
-        for(int j=0; j<=sum1; j++){
-            if(arr[i]<j){
-                t[i][j]=t[i-1][j-arr[i-1]]+t[i-1][j];
-            }
-            else{
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=sum; j++){
+            if(arr[i-1]>j){
                 t[i][j]=t[i-1][j];
             }
+            else if(arr[i-1]<=j){
+                t[i][j]=t[i-1][j]+t[i-1][j-arr[i-1]];
+            }
         }
     }
-    return t[n][sum1];
+    return t[n][sum];
 
-}
-
-int count(int arr[],int n,int k,int range){
-    int sum1,sum2;
-    range=sum1+sum2;
-    k=sum1-sum2;
-    sum1=(range+k)/2;
-
-    int x= subsetcount(arr,n,sum1);
-    return x;
 }
 
 int main(){
@@ -47,6 +37,8 @@ int main(){
     for(int i=0; i<n; i++){
         sum+=arr[i];
     }
-    cout<<count(arr,n,diff,sum);
+
+    int s1=(sum+diff)/2;
+    cout<<subsetcount(arr,n,s1);
     return 0;
 }
