211ffc169438f9806c1b26d19daa5af16f46238b
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-03-21 08:48:28 +0530

    DP

---
+++ Longest_common_subsequence/25_min_operations_a_to_b.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define vi vector<int>
+#define pii vector<pair<int,int>>
+int dp[1001][1001];
+
+int LCS(string x,string y,int m ,int n){
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || j==0){
+                dp[i][j]=0;
+            }
+        }
+    }
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            if(x[i-1]==y[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
+int main(){
+    string x="abcef";
+    string y="abxy";
+    int lcs=LCS(x,y,x.length(),y.length());
+    int in=y.length()-lcs;
+    int del=x.length()-lcs;
+    cout<<"Total no of insertions "<<in<<endl;
+    cout<<"Total no of deletions "<<del<<endl;
+    return 0;
+}
