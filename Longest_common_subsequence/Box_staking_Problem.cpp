c5907ac04299d6d90726a92b844b4dac64f49ac9
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-03-31 13:43:28 +0530

    dp

--- Longest_common_subsequence/Box_staking_Problem.cpp
+++ Longest_common_subsequence/Box_staking_Problem.cpp
@@ -8,10 +8,11 @@ class box{
     int l,w,h;
 };
 bool compare(box a,box b){
-    return a.l*a.w<b.l*b.w;
+    return a.l*a.w>b.l*b.w;
 }
 int maxHeight(int length[], int width[],int height[],int n){
-    box arr[3*n];
+    int len=3*n;
+    box arr[len];
     for(int i=0; i<n; i++){
         int idx=0;
         arr[idx].h=height[i];
@@ -28,21 +29,36 @@ int maxHeight(int length[], int width[],int height[],int n){
         idx++;
         
     }
-    int len=3*n;
+    
     sort(arr,arr+len,compare);
     int lis[len];
 
     for(int i=0; i<len; i++){
         lis[i]=arr[i].h;
+        
     }
     for(int i=1; i<len; i++){
         for(int j=0; j<i; j++){
-            if(lis[i]<arr[])
+            if((arr[i].l<arr[j].l) && (arr[i].w<arr[j].w) && (lis[i]<lis[j]+arr[i].h)){
+                lis[i]=lis[j]+arr[i].h;
+            }
         }
     }
+    int mx=0;
+    for(int i=0; i<len; i++){
+        if(lis[i]>mx){
+            mx=max(mx,lis[i]);
+        }
+        
+    }
+    return mx;
 
 }
 int main(){
-    
+    int l[]={10,4,4};
+    int w[]={12,6,5};
+    int h[]={32,7,6};
+    int n=3;
+    cout<<maxHeight(l,w,h,n);
     return 0;
 }
