45454d32390d91cba75a2711fc55394c1173029a
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-04-01 12:01:26 +0530

    Leetcode 918

---
+++ Kadane_algo/max_sum_circular_subarray.cpp
@@ -0,0 +1,43 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define vi vector<int>
+#define pii vector<pair<int,int>>
+int maxSum(int arr[],int n){
+    int mx=INT_MIN;
+    int mxt=0;
+    int total=0;
+    int mn=INT_MAX;
+    int mnt=0;
+    
+    for(int i=0; i<n; i++){
+        total=total+arr[i];
+        mxt=mxt+arr[i];
+        if(mxt>mx){
+            mx=mxt;
+        }
+        
+        if(mxt<0){
+            mxt=0;
+        }
+        
+        int mnt=mnt+arr[i];
+        if(mn>mnt){
+            mn=mnt;
+        }
+        if(mnt>0){
+            mnt=0;
+        }
+        
+    }
+    if(total==mn){
+        return mn;
+    }
+    int ans=total-(mn);
+    return ans;
+}
+int main(){
+    int arr[]={-1,-2,-3,-2,-6};
+    int n=5;
+    cout<<maxSum(arr,n);
+    return 0;
+}
