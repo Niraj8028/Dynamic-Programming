9a0c703b2ab8e2c2d2bd2da11d40b138d167db12
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-04-04 13:53:03 +0530

    leetcode 85

--- DP_on_Grids/largest_rect_in_histogram.cpp
+++ DP_on_Grids/largest_rect_in_histogram.cpp
@@ -28,12 +28,37 @@ vector<int>l(vector<int>&histo){
     }
     return left;
 }
+vector<int>r(vector<int>&histo){
+    int n=histo.size();
+    stack<int>st;
+    vector<int>right;
+    for(int i=n-1; i>=0; i--){
+        if(st.empty()){
+            right.push_back(n-1);
+            st.push(i);
+        }
+        else{
+            while(!st.empty() && histo[st.top()]>=histo[i]){
+                st.pop();                
+            }
+            if(st.empty()){
+                right.push_back(n-1);
+            }
+            else{
+                right.push_back(st.top()-1);
+                st.push(i);
+            }
+        }
+        
+    }
+    return right;
+}
 int rectangle(vector<int>&histo){
 
 }
 int main(){
     vector<int>histo={2,1,5,6,2,3};
-    vector<int>ans=l(histo);
+    vector<int>ans=r(histo);
     for(int i=0; i<ans.size(); i++){
         cout<<ans[i];
     }
